26bd8075425c24454b5eacaa205afb78513c76da
Hashimoto1a <newtondainercia@gmail.com>
2026-05-20 14:33:15 -0300

    Class position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program{
 	public static void main(String[] args) {
 		
 		System.out.println("hello, world!");
 		
+		
+		Position pos = new Position(1, 1);
+		
+		System.out.println(pos);
 	}
 }
