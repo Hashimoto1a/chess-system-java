91182ab22af116c633f2737e47a9c5804bbb243c
Hashimoto1a <newtondainercia@gmail.com>
2026-05-27 17:09:56 -0300

    Handling exceptions and clearing screen

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -44,7 +44,7 @@ public class ChessMatch {
 	
 	private void validateSourcePosition(Position position) {
 		if (!board.thereIsAPiece(position)){
-			throw new BoardException("Position not on board");
+			throw new ChessException("There is no piece on source position");
 		}
 	}
 	
