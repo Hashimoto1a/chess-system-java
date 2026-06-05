98558c9dd1cfd1b99fc619f642b47228b9fc710d
Hashimoto1a <newtondainercia@gmail.com>
2026-06-05 15:34:06 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -69,7 +69,9 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
 		System.out.println("Wainting player: " + chessMatch.getCurrentPlayer());
-		
+		if(chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 		
 	}
 	
@@ -102,6 +104,9 @@ public class UI {
 	}
 	
 	private static void printCapturedPieces(List<ChessPiece> captured) {
+		for (ChessPiece piece : captured) {
+		    System.out.println(piece);
+		}
 		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
 		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
 		System.out.println("Captured pieces: ");
